ddfc0ece140e8d39c74af78e7c20ea7d4c4ce903
irjala <jonasirjala@gmail.com>
2021-02-22 15:37:51 +0200

    Login rework (En tredjedel färdig)

--- projekt2/login.php
+++ projekt2/login.php
@@ -12,36 +12,51 @@ session_start();
 
 <?php
 
-  if (isset($_POST['usr']) && isset($_POST['psw'])) //when form submitted
+  if (isset($_POST['usr']) && isset($_POST['psw']) && isset($_POST['submit'])) //when form submitted
   {
-  $name = stripslashes($_POST["usr"]); 
-  $name = mysql_real_escape_string($_POST["usr"]);
-  $password = stripslashes($_POST["psw"]); 
-  $password = mysql_real_escape_string($_POST["psw"]); 
+    if(empty(trim($_POST["usr"]))){
+      $usr_error = "Please enter username.";
+    } else{
+      $name = trim($_POST["usr"]);
+      $name = stripslashes($name);
+    }
+    if(empty(trim($_POST["psw"]))){
+      $psw_error = "Please enter password.";
+    } else{
+      $password = trim($_POST["psw"]);
+      $password = stripslashes($name);
+    }
 
 
   $sql = ("SELECT username, password FROM users WHERE username = '".$name."' AND  password = '".$password."'");
-  if(mysql_num_rows($result1) > 0 )
+  if(mysql_num_rows($sql) > 0 )
     {
     print("YAY SUCCESS");
+
+    $_SESSION['user'] = $name;
+
       /*  
-  $stmt = $conn->prepare($sql); 
-  $stmt->bind_param("ss", $username, $password);
-  $stmt->execute();
-  $result = $stmt->get_result(); // get the mysqli result
-  $user = $result->fetch_assoc();
+    $stmt = $conn->prepare($sql); 
+    $stmt->bind_param("ss", $username, $password);
+    $stmt->execute();
+    $result = $stmt->get_result(); // get the mysqli result
+    $user = $result->fetch_assoc();
 
 
-  if ($_POST['login'] === login && $_POST['password'] === password)
-  {
+    if ($_POST['login'] === login && $_POST['password'] === password)
+    {
     $_SESSION['login'] = $_POST['login']; //write login to server storage
     header('Location: /'); //redirect to main */
+    }
+    else {
+      echo "<script>alert('Give both username and password');</script>";
+      echo "<noscript>Give both username and password</noscript>";
     }
   }
   else if (!isset($_POST['usr']) || !isset($_POST['psw']))
   {
-    echo "<script>alert('Give both username and password');</script>";
-    echo "<noscript>Give both username and password</noscript>";
+    print("Fyll i både användarnamn och lösenord tack!");
+    
     //echo "<script>alert('Wrong login or password');</script>";
     //echo "<noscript>Wrong login or password</noscript>";
   }

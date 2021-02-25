eea65f06f39d122c1f69ce01852a35106f756c53
NikeMan-San <westinni@arcada.fi>
2021-02-25 22:40:35 +0200

    cringe

--- projekt2/index.php
+++ projekt2/index.php
@@ -4,7 +4,10 @@
 
 <article>
     <img src="../projekt2/images/love.jpg" width="450px" height="200px" id="love">
+    <br>
+    <br>
     <h1>Välkommen till Kiss Kiss Bang Bang!</h1>
+    <br>
     <?php
     if (!isset($_SESSION['user'])){
         echo("<p>Du är för tillfället inte inloggad</p>");
@@ -15,6 +18,11 @@
 
     ?>
 </article>
+<blockquote><img src="../projekt2/images/quote.jpg" alt="tadadadada...im loving it" width="400" height="250">
+<p id="quttis"></p>När vi treffade på Kiss Kiss Bang Bang så var det kärlek på första ögonblicket. Nu har vi varit lyckliga tillsammans redan 3 veckor!!
+</p>
+
+</blockquote>
 <article>
 <?php include "profile.php" ?>
 <?php include "comment.php" ?>

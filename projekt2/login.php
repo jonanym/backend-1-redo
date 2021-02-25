5958246a2f5b081d134c554108338533c1e152e4
irjala <jonasirjala@gmail.com>
2021-02-25 10:44:28 +0200

    Login hash version (almost fixed)

--- projekt2/login.php
+++ projekt2/login.php
@@ -12,6 +12,10 @@
 
   if (isset($_POST['usr']) && isset($_POST['psw'])) //when form submitted
   {
+    if($_POST['usr'] == $_POST['psw']){
+      echo("<p>OBS fälten får inte vara tomma eller samma!</p>");
+    } else {
+
     if(empty(trim($_POST["usr"]))){
       $usr_error = "Du kan inte lämna användarnamn tomt.";
       print($usr_error);
@@ -30,17 +34,36 @@
       print("PW klar ");
     }
     // removed från query temporärt: ("SELECT username, password FROM users WHERE username AND  password = '".md5($password)."');
+  
+    print($password);
+  $hashedpassword = hash("sha256", $password);
+  echo("<p>".$password."</p>");
   $conn = create_conn();
-  $query = "SELECT username, password FROM users WHERE username AND password = (?, ?)";
+  $query = "SELECT * FROM users WHERE username = ?";
   $stmt = $conn->prepare($query);
-  $stmt->bind_param("ss", $name, $password);
+  $stmt->bind_param("s", $name);
   $stmt->execute();
+  if ($stmt->execute()) { 
+    print("EXECUTED");
+ } else {
+    print("Not executed");
+ }
   $result = $stmt->get_result();
-  $row = fetch_assoc($result);
-
-  print("före hashingen! ");
-
-  $password = hash("sha256", $password);
+  print("get result");
+  $row = mysqli_fetch_assoc($result);
+  print("fetched assoc");
+  echo("<br><br>");
+  print($hashedpassword);
+  echo("<br>");
+  print($row['password']);
+  if($hashedpassword==$row['password']){
+    print("Success");
+    $_SESSION['user'] = $name;
+    header('Refresh:2; url=index.php');
+  } else {
+    echo "<div class='form'>
+   <h3>Username/password is incorrect.</h3></div>";
+  }
   
   /*$pwquery = "SELECT password FROM users WHERE password = ?";
   $pwstmt = $conn->prepare($pwquery);
@@ -48,21 +71,14 @@
   $pwstmt->execute();
   $pwresult = $pwstmt->get_result();
   $pwrow = mysqli_num_rows($pwresult); */
-
-  if($name == $row['username'] && $password==$row['password']){
-    print("Success");
-    $_SESSION['user'] = $name;
-    header('Refresh:2; url=index.php');
-  } else {
-    echo "<div class='form'>
-   <h3>Username/password is incorrect.</h3></div>";
-    }
+  } 
+  
   }else{ 
     echo "<h3>Användarnamn eller lösenord fanns ej</h3></div>";
-
+  
 
   ?><div class="box">
   <p>Fyll i fältet ovanför och submit för att denna låda skall försvinna</p>
   </div>
   <?php } ?>
-  <?php include "footer.php" ?>
+  <!-- < ?php include "footer.php" ?> --> 

0d2dd42e76c3156d8a4410929c2f68dad2c232e6
NikeMan-San <westinni@arcada.fi>
2021-02-25 20:33:25 +0200

    raport+diagrambild

--- projekt2/rapport.php
+++ projekt2/rapport.php
@@ -5,6 +5,18 @@
 <article>
     <img src="../projekt2/images/fighter.jpg" width="450px" height="200px" id="love">
     <h3>Vår webbrapport. Niklas den slöa, Jonathan den unga och Jonas the legend!</h1>
+    <img src="../projekt2/images/phpmyadmindesign.PNG">
+    <br>
+    <li>SELECT används för att välja data från en databas. Data som returneras lagras i en resultattabel.</li>
+    <li>INSERT INTO används för att infoga nya poster i en tabell.</li>
+    <li>UPDATE används för att modifiera befintliga poster i en tabell.</li>
+    <li>DELETE används för att ta bort befintliga poster i en tabell.</li>
+    <li>WHERE används för att filtrera poster. WHERE används för att extrahera endast de poster som uppfyller ett angivet villkor.</li>
+    <li>LIKE används i en WHERE-sats för att söka efter ett angivet mönster i en kolumn.</li>
+    <li>ORDER BY används för att sortera resultatuppsättningen i stigande ordning som standard.
+    <li>För att sortera posterna i fallande ordning använder du nyckelordet ORDER BY | DESC.</li></li> 
+        
+
     
 
 </article>

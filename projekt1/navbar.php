7d42535aa90622ac70ee3b589aa6df909f4550c6
jonanym <nymajona@arcada.fi>
2021-04-14 21:19:06 +0300

    Projekt 1 redo helt done

--- projekt1/navbar.php
+++ projekt1/navbar.php
@@ -2,5 +2,6 @@
             <!-- Logo och meny finns i nav -->
             <ul>
                 <a href="../projekt1/">Projekt 1</a>
+                <a href="./raport.php">Raport</a>
             </ul>
         </nav>
---
+++ projekt1/raport.php
@@ -0,0 +1,36 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Jonathans JS Demo</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <!-- Containern har max bredd 800px -->
+    <div id="container">
+
+        <?php include "navbar.php" ?>
+
+        <!-- Artiklar placerar sig snyggt efter varann -->
+        <article>
+    <h1>Rapport</h1>
+        <p>Andra försöket på kursen gick myckke bättre. Det känns iallafall som att jag förstod allting lite bättre då jag gjorde det andra gången</p>
+        <p>Jag behöver säkert inte skriva en så lång rapport eftersom att den är ganska samma som i mitt första försök på projekt</p>
+        <p>Jag kunde använda endel uppgifter pånytt men förbättra allting. Det var skönt att jobba i par men ledde ganksa långt till det att det jag inte kunde lät jag dom som kunde göra och så lärde jag mig ingenting</p>
+        <p>Nu borde iallafall allting fungera och inte är det j8u så svårt att få saker att fungera men jag skulle gärna öva mer på felsökning och fixande av problem eftersom de oftast orakar största besvären</p>
+        <p>Hadde probelm med att kolla om datumet är i det förflutna eller i framtiden men det var ju en idiot enkel lösning att bara laga datumena med mktime så de är en siffersträn istället för att på samma gång formatera med date och rådda till allting</p>
+        <p>Det är ofta de mest simpla lösnignarna som tar längst med jag lärde mig myckke nytt med att göra projekte round 2</p>
+    </article>
+    </div>
+</body>
+
+<!-- Script kan köras efter att sidan laddats in -->
+<script src="script.js"></script>
+
+
+</html>
